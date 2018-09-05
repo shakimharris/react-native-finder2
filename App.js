413d1058c9e631fc9e8fb677c4fd54ba61270adb
amenraprod <amenraprod@gmail.com>
2018-09-04 22:12:47 -0400

    added blank state and textinput

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+state = {
+  name: ''
+}
+
+
+
   render() {
     return (
       <View style={styles.container}>
