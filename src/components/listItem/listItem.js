6137198aefb36e8a6bde00dc413f501707571283
amenraprod <amenraprod@gmail.com>
2018-09-05 00:23:52 -0400

    imported stylesheet

--- src/components/listItem/listItem.js
+++ src/components/listItem/listItem.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 const listItem = () => (
+<View>
+<Text></Text>
+
+</View>
 
 );
 
